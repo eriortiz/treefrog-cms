abd6dbd6f8b28995cf032138fc945ff13158b563
eriortiz <54683345+eriortiz@users.noreply.github.com>
2020-02-17 18:08:26 -0500

    changes

--- public/app/app.js
+++ public/app/app.js
@@ -1,13 +1,32 @@
-function initButtons () {
-    $('.get-started').click(function (e) {
-        $('#home div').removeClass('active');
-        $('#addNav div').addClass('active');
+$(document).ready(function()  
+{
+    initButtons();
+})
 
-        $('.text-wrapper').html(TREEFROG_SERVICE.getGetStartedContent());
-        $('.btn-holder').html(TREEFROG_SERVICE.getGetStartedContent());
+function initButtons()
+{
+    $(".get-started").click(function()
+    {
+        $("#home div").removeClass("active");
+        $("#addNav div").addClass("active");
+
+        $(".text-wrapper").html(TREEFROG_SERVICE.getGetStartedContent());
+        $(".btn-holder").html(TREEFROG_SERVICE.getCreateNavButtons());
+
+        mainNavButtonClick();
+        subNavButtonClick();
+
+        initButtons();
+    });
+    
+    $("#home").click(function() 
+    {
+        $("#addNav div").removeClass("active");
+        $("#home div").addClass("active");
+
+        $(".text-wrapper").html(TREEFROG_SERVICE.getHomeContent());
+        $(".btn-holder").html(TREEFROG_SERVICE.getHomeStartButton());
+
+        initButtons();
     });
 }
-
-$(document).ready(function() {
-    initButtons();
-});
--- public/app/service.js
+++ public/app/service.js
@@ -1,19 +1,65 @@
-var TREEFROG_SERVICE = (function() {
+let TREEFROG_SERVICE = (function() 
+{
 
-    var _getGetStartedContent = function() {
-
+    let _getGetStartedContent = function()
+    {
+        let contentStr = `
+            <h1>Get Started Content</h1>
+            <p>This is the screen where you will create your navigation and page content.</p>
+            <p>First, you will need to create a main navigation. Once you have created a main navigation you can create a sub-navigation if you would like to.</p>
+            <p>Once you create either a nav or sub-nav a text editor will pop up and you will be allowed to create your page content.</p>
+        `;
 
         return contentStr;
-    };
+    }
 
-    var _getCreateNavButtons = function() {
+    let _getCreateNavButtons = function()
+    {
+        let buttonString = `
+            <span class="btn btn-dark main-nav">Create Main Nav</span>
+            <span class="btn btn-dark sub-nav">Create Sub Nav</span>
+        `;
 
+        return buttonString;
+    }
+
+    let _getHomeContent = function()
+    {
+        let contentStr = `
+            <h1>Welcome to the Treefrog CMS</h1>
+            <p>
+            Here you will create your content for your webpages. You won't be able
+            to create all page elements but only the content for the page.
+            </p>
+
+            <p>
+            You must first create the navigation. Once you have the navigation
+            created you can add page content and publish the page. You can even
+            add sub navigation as well.
+            </p>
+
+            <p>
+            Your fist step is to click on the Add Navigation link and add your
+            first navigation link.
+            </p>
+        `;
 
         return contentStr;
-    };
+    }
+
+    let _getHomeStartButton = function()
+    {
+        let buttonString = `
+            <span class="btn btn-dark get-started">Get Started</span>
+        `;
+
+        return buttonString;
+    }
 
     return {
-        getGetStartedContent
-    };
-
+        getGetStartedContent: _getGetStartedContent,
+        getCreateNavButtons: _getCreateNavButtons,
+        getHomeContent: _getHomeContent,
+        getHomeStartButton: _getHomeStartButton
+    }
 })();
