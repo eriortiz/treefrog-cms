d31aa2481ef4c33a95f04f86c0b253a56681d647
eriortiz <54683345+eriortiz@users.noreply.github.com>
2020-02-24 17:15:07 -0500

    ddd

--- public/app/app.js
+++ public/app/app.js
@@ -43,3 +43,26 @@ function initButtons()
         initButtons();
     });
 }
+
+function startQuill()
+{
+    var toolbarOptions = [
+        ['bold', 'italic', 'underline', 'strike'],        
+        ['blockquote', 'code-block'],
+      
+        [{ 'header': 1 }, { 'header': 2 }],               
+        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+        [{ 'script': 'sub'}, { 'script': 'super' }],      
+        [{ 'indent': '-1'}, { 'indent': '+1' }],          
+        [{ 'direction': 'rtl' }],                         
+      
+        [{ 'size': ['small', false, 'large', 'huge'] }],  
+        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+      
+        [{ 'color': [] }, { 'background': [] }],          
+        [{ 'font': [] }],
+        [{ 'align': [] }],
+      
+        ['clean']                                         
+      ];
+    }
