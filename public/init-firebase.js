cdc61958f4896362c8b1728a83d1fb56e1f0b717
eriortiz <54683345+eriortiz@users.noreply.github.com>
2020-04-05 16:51:38 -0400

    aaa

--- public/init-firebase.js
+++ public/init-firebase.js
@@ -1,16 +1,14 @@
-<script>
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyBj6_GQ7jZlxGKrsRmfKmUaAmg1ytCk3QE",
-    authDomain: "treefrog-cms-62c70.firebaseapp.com",
-    databaseURL: "https://treefrog-cms-62c70.firebaseio.com",
-    projectId: "treefrog-cms-62c70",
-    storageBucket: "treefrog-cms-62c70.appspot.com",
-    messagingSenderId: "49500609786",
-    appId: "1:49500609786:web:33d59665e1eeaca7b93d43",
-    measurementId: "G-2WQ2ZNVWWK"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-</script>
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: 'AIzaSyBj6_GQ7jZlxGKrsRmfKmUaAmg1ytCk3QE',
+  authDomain: 'treefrog-cms-62c70.firebaseapp.com',
+  databaseURL: 'https://treefrog-cms-62c70.firebaseio.com',
+  projectId: 'treefrog-cms-62c70',
+  storageBucket: 'treefrog-cms-62c70.appspot.com',
+  messagingSenderId: '49500609786',
+  appId: '1:49500609786:web:33d59665e1eeaca7b93d43',
+  measurementId: 'G-2WQ2ZNVWWK',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
